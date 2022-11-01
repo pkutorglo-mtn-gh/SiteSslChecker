9eb60159ffa39b6269d0c61cac50f41601014fd6
pkutorglo-mtn-gh <109282374+pkutorglo-mtn-gh@users.noreply.github.com>
2022-11-01 14:38:32 +0000

    Updated cert_expiry_checker.py
    Added comments to the file

--- cert_expiry_checker.py
+++ cert_expiry_checker.py
@@ -1,5 +1,5 @@
-import socket
-import ssl
+import socket  # to send messages across networks
+import ssl     #
 import datetime
 
 
@@ -7,13 +7,15 @@ class ssl_check():
 
     def __init__(self, hostname):
         print(hostname)
-        # https://www.w3schools.com/python/python_datetime.asp
+        # date format from ww3sch
         ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
         context = ssl.create_default_context()
-        # to wrap a socket.
+        # to wrap a socket., as SSLSocket objects
+        # AF_INET for ipv4 representation
         conn = context.wrap_socket(socket.socket(socket.AF_INET),server_hostname=hostname,)
         conn.settimeout(3.0)
         conn.connect((hostname, 443))
+        # to  retrieve the SSL certificate from the other endpoint of the communication
         ssl_info = conn.getpeercert()
         Exp_ON=datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
         Days_Remaining= Exp_ON - datetime.datetime.utcnow()
